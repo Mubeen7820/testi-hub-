7949111c4588a56f8b60210a896b64dcdd1b4215
Mubeen <mubeenahmed7820@gmail.com>
2026-04-29 01:11:19 +0530

    Configure standard Vercel api/ entry point for backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -72,3 +72,5 @@ process.on('uncaughtException', (err) => {
 process.on('unhandledRejection', (reason, promise) => {
   console.error('Unhandled Rejection at:', promise, 'reason:', reason);
 });
+
+export default app;
